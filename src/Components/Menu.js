3ebd40a4abc20c0086c997fc140b1edfb459d729
Alexandar Valov <alexvulov123@gmail.com>
2022-07-25 23:06:13 +0300

    Added the homepage with all the available items

---
+++ src/Components/Menu.js
@@ -0,0 +1,53 @@
+import { StyledMenu } from "./Styles/MenuStyled";
+
+const Menu = ({ menu }) => {
+  return (
+    <StyledMenu className="menu active-content" id="menu">
+      <div className="section-center">
+        {menu.map((item) => (
+          <article className="menu-item" key={item.id}>
+            <div className="img-container">
+              <img src={item.img} className="photo" alt="" />
+
+              <div className="image-overlay image-overlay-blur">
+                <button
+                  className="btn overlay-btn"
+                  title="Add to shopping cart"
+                >
+                  <span>
+                    <i
+                      className="
+                      fa-solid fa-cart-shopping
+                      shoppingBag-icon
+                      overlay-icon
+                    "
+                    ></i>
+                  </span>
+                  Add to cart
+                </button>
+              </div>
+            </div>
+            <div className="item-info-right" title="Add to favourites">
+              <i
+                className={`btn fa-solid fa-heart heart-icon ${
+                  item.fav ? "fav" : ""
+                }`}
+              ></i>
+            </div>
+            <div className="item-info">
+              <h3>{item.name}</h3>
+              <h3 className="item-description">{item.desc}</h3>
+              <h3 className="colors">{item.color}</h3>
+              <h3 className="price">
+                {item.price}
+                <span className="currency"> {item.currency}</span>
+              </h3>
+            </div>
+          </article>
+        ))}
+      </div>
+    </StyledMenu>
+  );
+};
+
+export default Menu;
