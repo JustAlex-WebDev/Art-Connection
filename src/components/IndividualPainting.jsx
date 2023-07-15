5b4161d8da71d1109f4513724b3745f7bd6e8cf2
Alexandar Valov <alexvulov123@gmail.com>
2023-07-15 23:35:30 +0300

    Updated the Navbar Component and made most of the poster mockups for the IndividualPainting Component.

--- src/components/IndividualPainting.jsx
+++ src/components/IndividualPainting.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { motion as m } from "framer-motion";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
 const IndividualPainting = ({
   item,
@@ -10,6 +12,8 @@ const IndividualPainting = ({
   addItemShoppingCart,
   removeItemShoppingCart,
 }) => {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <m.div
       initial={{ opacity: 0 }}
@@ -17,14 +21,70 @@ const IndividualPainting = ({
       transition={{ delay: 2.5, duration: 0.5 }}
       className="main-div my-24 xxsm:mt-8 flex flex-col md:flex-row justify-center"
     >
-      <div className="flex justify-center items-center">
-        <img
-          src={item.img}
-          alt={item.name}
-          loading="lazy"
-          className="w-[12rem] xxsm:w-[17rem] md:w-[25rem] lg:w-[27rem] object-scale-down h-[15rem] xxsm:h-[20rem] md:h-[30rem] lg:h-[35rem] shadow-lg bg-secondary hover:animate-panImage hover:cursor-pointer duration-300"
-        />
-      </div>
+      {theme === "dark" ? (
+        <div className="flex justify-center items-center gap-4 w-full md:w-[25rem] lg:w-[27rem] h-[20rem] xxsm:h-[30rem] md:h-[32rem] lg:h-[35rem]">
+          <div className="flex flex-col justify-center items-center gap-4 w-[50%] h-[95%]">
+            <img
+              loading="lazy"
+              src={item.poster2Dark}
+              alt={item.poster2Dark}
+              className="w-full h-[45%] object-cover rounded-tl-2xl rounded-md shadow-sm hover:opacity-50 duration-300 shadow-white"
+            />
+            <img
+              loading="lazy"
+              src={item.poster4Dark}
+              alt={item.poster4Dark}
+              className="w-full h-[45%] object-cover rounded-bl-2xl rounded-md shadow-sm hover:opacity-50 duration-300 shadow-white"
+            />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-4 w-[50%] h-[95%]">
+            <img
+              loading="lazy"
+              src={item.poster1Dark}
+              alt={item.poster1Dark}
+              className="w-full h-[45%] object-cover rounded-tr-2xl rounded-md shadow-sm hover:opacity-50 duration-300 shadow-white"
+            />
+            <img
+              loading="lazy"
+              src={item.poster3Dark}
+              alt={item.poster3Dark}
+              className="w-full h-[45%] object-cover rounded-br-2xl rounded-md shadow-sm hover:opacity-50 duration-300 shadow-white"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center gap-4 w-full md:w-[25rem] lg:w-[27rem] h-[20rem] xxsm:h-[30rem] md:h-[32rem] lg:h-[35rem]">
+          <div className="flex flex-col justify-center items-center gap-4 w-[50%] h-[95%]">
+            <img
+              loading="lazy"
+              src={item.poster2Light}
+              alt={item.poster2Light}
+              className="w-full h-[45%] object-cover rounded-tl-2xl rounded-md shadow-md hover:opacity-50 duration-300"
+            />
+            <img
+              loading="lazy"
+              src={item.poster4Light}
+              alt={item.poster4Light}
+              className="w-full h-[45%] object-cover rounded-bl-2xl rounded-md shadow-md hover:opacity-50 duration-300"
+            />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-4 w-[50%] h-[95%]">
+            <img
+              loading="lazy"
+              src={item.poster1Light}
+              alt={item.poster1Light}
+              className="w-full h-[45%] object-cover rounded-tr-2xl rounded-md shadow-md hover:opacity-50 duration-300"
+            />
+            <img
+              loading="lazy"
+              src={item.poster3Light}
+              alt={item.poster3Light}
+              className="w-full h-[45%] object-cover rounded-br-2xl rounded-md shadow-md hover:opacity-50 duration-300"
+            />
+          </div>
+        </div>
+      )}
+
       <div className="w-[100%] md:w-[50%] pt-8 md:pt-0 pl-0 md:pl-8 lg:pl-16 text-primary overflow-hidden md:h-[30rem] lg:h-[35rem] flex flex-col justify-center gap-4 md:gap-4 lg:gap-8 items-center md:items-start text-center md:text-left">
         <div className="flex flex-col gap-2 md:gap-2 lg:gap-4">
           <div className="text-2xl md:text-3xl lg:text-4xl">{item.name}</div>
